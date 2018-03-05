72ccb8b835159d5a80b662903c4596bb21c012ba
Clayton Vis <clayton.vis@ucalgary.ca>
2018-03-05 14:18:16 -0700

    Initial Setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const port = 3000;
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function(){
+    console.log('listening on ', port);
+});
+
