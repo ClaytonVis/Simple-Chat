cf4c1bc8812443f44a416cd2a4466b94b7fc6455
Clayton Vis <clayton.vis@ucalgary.ca>
2018-03-10 02:45:03 -0700

    Fixed Messages

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ var path = require('path');
 var cookie = require('cookie');
 
 
-let counter = 0;
+let usrcount = 0;
 let userslist = {};
-let messagelist = [];
+const date = new Date();
+let messagequeue = [((date.getTime()) +  " System Welcome to the simple chatter. Say something.")];
+let msgCount = 0;
 
 app.set('views', path.join( __dirname, 'views'));
 app.set('view engine', 'jade');
@@ -20,45 +22,62 @@ app.use(cookieParser());
 
 app.get('/', function(req, res){
     if (!req.cookies["name"]) {
-        counter ++;
-        res.cookie("name", "User" + counter);
+        usrcount ++;
+        res.cookie("name", "User" + usrcount);
     }
     res.render('home', {
         users: userslist,
-        messages: messagelist
     });
- 
 });
+
 
 io.on('connection', function(socket){
     usrcook = cookie.parse(socket.request.headers.cookie || socket.handshake.headers.cookie);
     if (usrcook["name"]) {
         usrname = usrcook["name"];
     } else {
-        counter ++;
-        usrname = "User" + counter;
+        usrcount ++;
+        usrname = "User" + usrcount;
     }
 
+    socket.emit('new name', usrname);
+    socket.emit('init messages', messagequeue);
     console.log(usrname + ' has connected');
     userslist[usrname] = "online";
+    io.emit('update users', userslist);
+
 
     socket.on('disconnect', function(){
         console.log(usrname + ' has disconnected');
         userslist[usrname] = "offline";
+        io.emit('update users', userslist);
     });
 
-    socket.on('chat message', function(msg){
+
+    socket.on('submit message', function(msg){
         if (msg.split(' ')[0] == '/nick') {
             var newname = msg.slice(6);
             if (newname.length >= 16) {
                 console.log('name too long');
+            } else if (userslist[newname] == "true") {
+                socket.emit('name taken', newname);
+                console.log(usrname + ": name taken, " + newname);
             } else {
                 console.log(newname);
+                delete userslist[usrname];
+                userslist[newname] = "online";
                 usrname = newname;
                 socket.emit('new name', newname);
+                io.emit('update users', userslist);
             }
-        } else { 
-            io.emit('chat message', msg);
+        } else {
+            messagequeue.push(msg);
+            msgCount ++;
+            io.emit('recieve message', msg);
+            if (msgCount >= 200) {
+                messagequeue.shift();
+                msgCount --;
+            }
         }
     });
 });
--- views/home.js
+++ views/home.js
@@ -1,7 +1,9 @@
 var socket = io();
+var users;
+var usrname;
 if (document.cookie) {
     var cookieList = document.cookie.split(';');
-    var usrname = cookieList[cookieList.indexOf('name=')];
+    usrname = cookieList[cookieList.indexOf('name=')];
 } else {
     socket.emit('init name');
     
@@ -11,21 +13,70 @@ function alertMe() {
     alert("this works bro\n" + document.cookie);
 }
 
+function clearMe() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    alert("possibly cleared\n" + document.cookie);
+}
+
+
 $(function () {
     $('form').submit(function() {
-        console.log("test message");
-        socket.emit('chat message', $('#m').val());
+        console.log("Submit message: " + $('#m').val());
+        var date = new Date();
+        var time = date.getTime();
+        var mssg = time + " " + usrname + " " + $('#m').val();
+        socket.emit('submit message', mssg);
+       // socket.emit('submit message', (time + (usrname + ": " + $('#m').val())));
         $('#m').val('');
         return false;
     });
 
-    socket.on('chat message', function(msg) {
-        $('messages').append($('<li>').text(msg));
+    socket.on('init messages', function(messages) {
+        var cont;
+        for (var i = 0; i < messages.length; i++) {
+            cont = parseMssg(messages[i]);
+            $('#messages').append(cont);
+        }
+        
+    });
+
+    socket.on('recieve message', function(msg) {
+        console.log("Recieved message: " + msg);
+        var cont = parseMssg(msg);
+        if (cont) {
+            $('#messages').append(cont);
+        }
     });
 
     socket.on('new name', function(msg) {
         document.cookie = "name=" + msg;
-        $('#name').text(msg);
+        usrname = msg;
+        $('#name').text(usrname);
     });
 
+    socket.on('name taken', function(nw) {
+        alert("name taken: " + nw);
+    });
+
+    socket.on('update users', function(list) {
+        users = list;
+        $('#userWell').empty();
+        for (user in list) {
+            $('#userWell').append('<li>' + user + '</li>');
+        }
+    });
 });
+
+function parseMssg(mssg) {
+    if (mssg.toString() !== 0) {
+        var date = new Date();
+        splMsg = mssg.split(' ');
+        date.setTime(splMsg[0]);
+        var auth = splMsg[1];
+        var time = date.toLocaleTimeString();
+        var msgTxt = mssg.slice(splMsg[0].length + splMsg[1].length + 2);
+        return ('<li><span class="time">' + time + ' </span><span class="auth"> ' + auth + ': </span><span class="mssg"> ' + msgTxt + ' </span></li>');
+    } else {
+        return false;
+    }
+}
